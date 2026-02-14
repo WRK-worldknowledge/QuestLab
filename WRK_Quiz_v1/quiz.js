869aa3bad15b0dca5e09f2553747e75c95cb5a60
WRK-worldknowledge <mobilonka@gmail.com>
2026-02-14 14:56:29 +0100

    Update quiz.js
    V11

--- WRK_Quiz_v1/quiz.js
+++ WRK_Quiz_v1/quiz.js
@@ -4,7 +4,7 @@ let current = 0;
 let score = 0;
 
 // ================= LOAD DATA =================
-fetch("/QuestLab/WRK_Quiz_v1/data/wrk-data.json?v=10")
+fetch("/QuestLab/WRK_Quiz_v1/data/wrk-data.json?v=11")
   .then(r => r.json())
   .then(json => {
     data = json;
