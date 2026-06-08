4f57a4d4652d6d0e61b04f933e68b2c49edd61ad
WRK-worldknowledge <mobilonka@gmail.com>
2026-06-08 14:13:05 +0200

    Update setup.js

--- games/AIRIMP-match-learn/setup.js
+++ games/AIRIMP-match-learn/setup.js
@@ -1,82 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(){
   const lessonNames = {
 
-"Africa":[
-"Northern Africa - Sahara countries",
-"Northern Africa - Sahel countries",
-"Central Africa",
-"Southern Africa"
-],
-
-"Asia":[
-"Middle East & EurAsia",
-"Central Asia",
-"Far East"
-],
-
-"America & Oceania":[
-"Countries and capitals North America & Caribbean",
-"Cities North America",
-"Countries and cities South America",
-"Oceania"
-],
-
-"Eastern Europe":[
-"Austria, Hungary, Romania, Moldova & the Balkan",
-"Finland, Baltics, Russia, Belarus, Poland, Czech Republic, Slovakia & Ukraine",
-"Greece",
-"Turkey & Cyprus"
-],
-
-"Western Europe":[
-"Countries and capital cities",
-"Scandinavia, Germany & BeNeLux",
-"Spain & Portugal",
-"Switzerland & Italy",
-"UK, Ireland & France"
+"AIRIMP":[
+"Special Services",
+"Meals",
+"Passenger & Ticketing",
+"Operations",
+"Baggage",
+"Master Challenge"
 ]
 
 }
 
 const moduleFiles = {
 
-Africa:[
-"africa_northern_africa_-_sahara_countries.json",
-"africa_northern_africa_-_sahel_countries.json",
-"africa_central_africa.json",
-"africa_southern_africa.json"
-],
+const moduleFiles = {
 
-Asia:[
-"asia_middle_east_eurasia.json",
-"asia_central_asia.json",
-"asia_far_east.json"
-],
-
-"America & Oceania":[
-"amoc_countries_and_capitals_north_america_caribbean.json",
-"amoc_cities_north_america.json",
-"amoc_countries_and_cities_south_america.json",
-"amoc_oceania.json"
-],
-
-"Eastern Europe":[
-"euro_austria_hungary_romania_moldova_the_balkan.json",
-"euro_finland_baltics_russia_belarus_poland_czech_republic_slovakia_ukraine.json",
-"euro_greece.json",
-"euro_turkey_cyprus.json"
-],
-
-"Western Europe":[
-"eurw_countries_and_capital_cities.json",
-"eurw_scandinavia_germany_benelux.json",
-"eurw_spain_portugal.json",
-"eurw_switzerland_italy.json",
-"eurw_uk_ireland_france.json"
+"AIRIMP":[
+"airimp_services.json",
+"airimp_meals.json",
+"airimp_passenger.json",
+"airimp_operations.json",
+"airimp_baggage.json",
+"airimp_master.json"
 ]
 
 }
-
 const modeSelect = document.getElementById("modeSelect")
 const moduleSelect = document.getElementById("moduleSelect")
 const lessonSelect = document.getElementById("lessonSelect")
