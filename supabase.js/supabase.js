a8764220423c95b8e688cbe01e4c02f40b3f8158
QuestLab <256198287+WRK-worldknowledge@users.noreply.github.com>
2026-07-15 08:08:46 +0200

    Update supabase.js

--- supabase.js/supabase.js
+++ supabase.js/supabase.js
@@ -4,7 +4,8 @@ const supabaseUrl =
 const supabaseKey =
     'sb_publishable_tC3c3BMT5jMdha5CBGMaNA_Osx70V3Q';
 
-const supabase = window.supabase.createClient(
-    supabaseUrl,
-    supabaseKey
-);
+const client =
+    window.supabase.createClient(
+        supabaseUrl,
+        supabaseKey
+    );
