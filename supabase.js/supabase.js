f5a091eb3eac5de7159dcfd03de7dfdfc86e6d2f
QuestLab <256198287+WRK-worldknowledge@users.noreply.github.com>
2026-07-15 07:50:56 +0200

    Create supabase.js

---
+++ supabase.js/supabase.js
@@ -0,0 +1,10 @@
+const supabaseUrl =
+    'https://kwndokcgtzboteszodqt.supabase.co';
+
+const supabaseKey =
+    'sb_publishable_tC3c3BMT5jMdha5CBGMaNA_Osx70V3Q';
+
+const supabase = window.supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
