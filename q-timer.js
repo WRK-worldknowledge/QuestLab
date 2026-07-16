ef02ece457f76978419e92a23a14326991472806
QuestLab <256198287+WRK-worldknowledge@users.noreply.github.com>
2026-07-16 20:43:50 +0200

    Create q-timer.js

---
+++ q-timer.js
@@ -0,0 +1,22 @@
+let seconds = 480;
+
+setInterval(() => {
+
+    seconds--;
+
+    const min =
+        String(
+            Math.floor(seconds/60)
+        ).padStart(2,"0");
+
+    const sec =
+        String(
+            seconds%60
+        ).padStart(2,"0");
+
+    document.getElementById(
+        "qTime"
+    ).innerHTML =
+        min + ":" + sec;
+
+},1000);
