b1944c330e4f1905bc04203a6462c5a5eb30f8b8
QuestLab <256198287+WRK-worldknowledge@users.noreply.github.com>
2026-07-19 09:22:14 +0200

    Refactor setup.js for better readability
    Refactor setup.js to improve readability and structure.

--- games/airline-quest/setup.js
+++ games/airline-quest/setup.js
@@ -3,28 +3,50 @@ document
     .onclick = () => {
 
     const type =
-    document.getElementById("gameType").value;
+        document.getElementById(
+            "gameType"
+        ).value;
 
-const user =
-    localStorage.getItem("questlabUser");
+    const demo =
+        new URLSearchParams(
+            location.search
+        ).get("demo") === "true";
 
-let dataset =
-    "airline_complete.json";
+    if(demo){
 
-if(
-    user &&
-    (
-        user.endsWith("@talnet.nl") ||
-        user.endsWith("@rebergen.org")
-    )
-){
-    dataset =
-        "airline_rocva_full_array.json";
-}
+        location.href =
+            "game.html?demo=true&type=" +
+            type;
 
-location.href =
-    "game.html?data=" +
-    dataset +
-    "&type=" +
-    type;
+        return;
+    }
+
+    const user =
+        localStorage.getItem(
+            "questlabUser"
+        );
+
+    let dataset =
+        "airline_complete.json";
+
+    if(
+        user &&
+        (
+            user.endsWith(
+                "@talnet.nl"
+            ) ||
+            user.endsWith(
+                "@rebergen.org"
+            )
+        )
+    ){
+        dataset =
+            "airline_rocva_full_array.json";
+    }
+
+    location.href =
+        "game.html?data=" +
+        dataset +
+        "&type=" +
+        type;
 };
